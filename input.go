6d8fd4e43425b4e6faed8606eecfaa7bd37fbfd2
Ilya Isaev <me@ilyaisaev.com>
2018-12-06 21:55:01 +0700

    Add test files

--- input.go
+++ input.go
@@ -29,3 +29,16 @@ func readDockerStat(file string) dockerStat {
 	}
 	return ds
 }
+
+func readVegeta(file string) dockerStat {
+	var ds dockerStat
+
+	f, err := ioutil.ReadFile(file)
+	if err != nil {
+		fmt.Println("Can't read file :", file, "Error : ", err)
+	} else {
+		json.Unmarshal(f, &ds)
+		fmt.Println(ds)
+	}
+	return ds
+}
