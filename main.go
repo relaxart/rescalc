e3f091a274e59b23cdfb20df39f3d10a440f5d87
Ilya Isaev <me@ilyaisaev.com>
2018-12-09 19:31:07 +0700

    Update

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 	fmt.Println("ResCal processing ...")
 
 	c := ParseConfig("./config.yaml")
-	fmt.Println("Resource deviation:", c.Deviation, "%")
+	fmt.Printf("Resource deviation: %f%", c.Deviation)
 	result := os.Args[1:]
 	if len(result) < 1 {
 		fmt.Println("You must pass results")
